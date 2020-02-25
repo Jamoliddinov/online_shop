df31be37a9f5edd30c3d05e2e570f2ae09f2067f
Madina <mokik0808@gmail.com>
2020-02-25 15:30:49 +0500

    wrote some views to userprofile

--- userProfile/views.py
+++ userProfile/views.py
@@ -1,14 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.loader import get_template
+from django.urls import reverse
 
 
-def login(request):
-    return HttpResponse()
+def login_list(request):
+    if request.method == 'GET':
+        template = get_template('shop/login.html')
+        body = template.render({})
+        return HttpResponse(body)
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            # sucessful login
+            login(request, user)
+            return HttpResponseRedirect(reverse('product_list'))
+        else:
+            return HttpResponseRedirect(reverse('login'))
 
 
 def register(request):
     return HttpResponse()
 
 
-def logout(request):
-    return HttpResponse()
+class LogoutView(View):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse('login'))
+
